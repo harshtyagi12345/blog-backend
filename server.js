c9737e90103fe5c8d255ab13c256014471bd0bf2
harshtyagi12345 <tyagiharshit96@gmaill.com>
2025-07-29 19:17:56 +0530

    frontend deploy

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const blogRoute = require("./router/blog-router")
 
 //teckle
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://blogging-platform-mu-three.vercel.app"
+    ],
     methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
     credential:true,
 }
